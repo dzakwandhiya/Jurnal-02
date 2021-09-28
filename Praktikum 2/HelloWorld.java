2483d20b9ff5fd514341a9d7a95ce27fd18844fe
dzakwandhiya <dzakwandhiya@student.telkomuniversity.ac.id>
2021-09-28 15:54:53 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+public class HelloWorld {
+	
+	public static void main(String[]args){
+		System.out.println("NAMA  : Dzakwan Dhiya Ulhaq!");
+		System.out.println("NIM   : 6706210092");
+		System.out.println("KELAS : 45-03");
+	}
+		
+}
